e751be095eb5948409f1c9dd772779114a4aa031
Long-Zixuan <778275146@qq.com>
2026-02-09 18:32:31 +0800

    贴图更换   LZX-2025-02-09-006

--- Assets/Scripts/Block/BlockDataManager.cs
+++ Assets/Scripts/Block/BlockDataManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 //这个了才是把纹理对应到Block上(字典中一一对应)
 public class BlockDataManager : MonoBehaviour
 {
-    public static float textureOffset = 0.01f;//一个很小的值用来防止贴图发生相互交错而出现伪影
+    public static float textureOffset = 0.001f;//一个很小的值用来防止贴图发生相互交错而出现伪影
     public static float tileSizeX, tileSizeY;
     public static Dictionary<BlockType, TextureData> blockTextureDataDictionary = new Dictionary<BlockType, TextureData>();
     public BlockDataSO textureData;
